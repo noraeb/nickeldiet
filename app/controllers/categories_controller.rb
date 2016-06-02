6f3259fd589ca51b02593f87efa2c0277fef91d0
noraeb <n.blomaard@gmail.com>
2016-06-02 16:58:10 +0200

    Show categories and products on index Page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,10 +2,11 @@ class CategoriesController < ApplicationController
 
   def index
     @categories = Category.all
+    @products = Product.all
   end
 
   def show
-    @category = Category.find( params[:id] )
     @categories = Category.all
+    @category = Category.find( params[:id] )
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,27 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @categories = Category.all
+    @product = Product.new
+    @product.category_id = params[:catgory_id]
+  end
+
+  def create
+      product = Product.new( product_params )
+
+      if product.save
+         redirect_to root_path
+      else
+         render new_product_path
+      end
+   end
+
+private
+
+   def product_params
+     params.require( :product ).permit( :name, :category_id )
+   end
+
 end
